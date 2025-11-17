84fb5e72698fbdb807adc9f8a5b0042794d85145
unknown <sabrinaramom09@gmail.com>
2025-11-17 14:19:07 -0300

    lojaspricio2.0

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -1,12 +1,14 @@
 const express = require('express');
-const  router = express.Router();
+const router = express.Router();
 
 // Referecia do arquivo de rotas
 
-const {clienteRoutes} = require('./clienteRoutes');
+const { clienteRoutes } = require('./clienteRoutes');
 const { produtoRoutes } = require('./produtoRoutes');
+const { pedidoRoutes } = require('./pedidoRoutes');
 
 router.use('/', clienteRoutes);
 router.use('/', produtoRoutes);
+router.use('/', pedidoRoutes);
 
 module.exports = { router };
